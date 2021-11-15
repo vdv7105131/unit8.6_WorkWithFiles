c8df640b6d35351b5efde7c6f17d8807b7df70f5
vdv7105131 <vdv7105131@gmail.com>
2021-11-15 17:10:31 +0300

    ok

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Task2
 {
@@ -6,7 +7,42 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Users\user\Desktop\111";
+            string path1 = @"C:\Users\user\Desktop\111\text.txt";
+            SizeFolder(path);
+        }
+
+        // Напишите программу, которая считает размер папки на диске
+        // (вместе со всеми вложенными папками и файлами). 
+        // На вход метод принимает URL директории, в ответ — размер в байтах.
+
+        static void SizeFolder(string path)
+        {
+            //DirectoryInfo sizeF = new DirectoryInfo(path);
+            //sizeF.GetFiles();       // получает список файлов
+            //sizeF.GetDirectories(); // получает список каталогов
+
+            string[] listFolder = Directory.GetDirectories(path); // получает список каталогов
+            foreach (string s in listFolder)
+            {
+                Console.WriteLine(s);
+            }
+                
+
+            string[] listFiles = Directory.GetFiles(path); // получает список файлов
+            long sumSize = 0;
+            foreach (string q in listFiles)
+            {
+                Console.WriteLine(q);
+                FileInfo sizeF = new FileInfo(q);
+                long a = sizeF.Length;  // размер файла
+                Console.WriteLine(a);
+                sumSize += a;
+            }
+            //преобразование long to byte
+            //byte[] byteArray = BitConverter.GetBytes(sumSize);
+            Console.WriteLine($"Размер в байтах: {sumSize}");
+
         }
     }
 }
