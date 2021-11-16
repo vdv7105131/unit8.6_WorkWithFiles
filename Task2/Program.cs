5a0873083bd0ed73818579cb524b441a737d8bc3
vdv7105131 <vdv7105131@gmail.com>
2021-11-16 13:29:42 +0300

    ok

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -8,41 +8,46 @@ namespace Task2
         static void Main(string[] args)
         {
             string path = @"C:\Users\user\Desktop\111";
-            string path1 = @"C:\Users\user\Desktop\111\text.txt";
-            SizeFolder(path);
+            
+            var q = GetDir(path);
+            GetFiles(q);
+
+
         }
 
         // Напишите программу, которая считает размер папки на диске
         // (вместе со всеми вложенными папками и файлами). 
         // На вход метод принимает URL директории, в ответ — размер в байтах.
 
-        static void SizeFolder(string path)
+        static string[] GetDir(string path)
         {
-            //DirectoryInfo sizeF = new DirectoryInfo(path);
-            //sizeF.GetFiles();       // получает список файлов
-            //sizeF.GetDirectories(); // получает список каталогов
-
-            string[] listFolder = Directory.GetDirectories(path); // получает список каталогов
-            foreach (string s in listFolder)
+            string[] listDir = Directory.GetDirectories(path); // получает список каталогов
+            foreach (string s in listDir)
             {
                 Console.WriteLine(s);
+                GetDir(s);
             }
-                
 
-            string[] listFiles = Directory.GetFiles(path); // получает список файлов
-            long sumSize = 0;
-            foreach (string q in listFiles)
+            return listDir;
+
+        }
+
+        static string[] GetFiles(string[] dir)
+        {
+            
+
+            for (int i = 0; i < dir.Length; i++)
             {
-                Console.WriteLine(q);
-                FileInfo sizeF = new FileInfo(q);
-                long a = sizeF.Length;  // размер файла
-                Console.WriteLine(a);
-                sumSize += a;
-            }
-            //преобразование long to byte
-            //byte[] byteArray = BitConverter.GetBytes(sumSize);
-            Console.WriteLine($"Размер в байтах: {sumSize}");
+                string[] listFiles = Directory.GetFiles(dir[i]); // получает список файлов
+                                                                 // 
+                FileInfo sizeF = new FileInfo(listFiles[i]);
+                long b = sizeF.Length;  // размер файла
+                Console.WriteLine(b);
+            }         
 
+
+            string[] a = {"", "" };
+            return a;
         }
     }
 }
